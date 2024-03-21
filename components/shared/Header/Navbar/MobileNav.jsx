017704dcb1c731c9b3f97a03bfe5c0386ac90ed0
JewelHFahim <jewelhfahim@gmail.com>
2024-03-21 13:23:51 +0600

    mobile nav updated

---
+++ components/shared/Header/Navbar/MobileNav.jsx
@@ -0,0 +1,72 @@
+import dynamic from "next/dynamic"; // Import dynamic from next/dynamic
+import React, { useState } from "react";
+import "react-modern-drawer/dist/index.css";
+import logo from "../../../../public/images/1ten365logo.png";
+
+// Import Drawer dynamically to disable SSR
+const Drawer = dynamic(() => import("react-modern-drawer"), { ssr: false });
+import { GiHamburgerMenu } from "react-icons/gi";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const { pathname } = router;
+
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  const menus = [
+    { title: "হোম", url: "/" },
+    { title: "কাস্টমার সার্ভিস", url: "" },
+    { title: "সাইট এডমিন", url: "/Admins/SiteAdmin" },
+    { title: "এডমিন", url: "/Admins/Admin" },
+    { title: "সাব-এডমিন", url: "/Admins/SubAdmin" },
+    { title: "সুপার-এজেন্ট", url: "/Admins/SuperAgent" },
+    { title: "এজেন্ট", url: "/Admins/Agent" },
+  ];
+
+  return (
+    <div className="">
+      <button onClick={toggleDrawer} className="text-2xl">
+        <GiHamburgerMenu />
+      </button>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="left"
+        className=""
+      >
+        <div className="px-4 py-2">
+          <div className="flex flex-col gap-y-10">
+            <div className="flex justify-center">
+              <Image width={120} height={90} src={logo} alt="" />
+            </div>
+
+            <div>
+              <ul className="flex flex-col gap-y-6">
+                {menus.map((item, i) => (
+                  <li key={i}>
+                    <Link
+                      href={item.url}
+                      className={`${
+                        pathname === item.url ? "bg-slate-100" : ""
+                      } px-5 py-1 rounded-md hover:bg-slate-100`}
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Drawer>
+    </div>
+  );
+};
+
+export default MobileNav;
--- components/shared/Header/Navbar/Navbar.jsx
+++ components/shared/Header/Navbar/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
   const { pathname } = router;
   console.log(pathname);
   return (
-    <div className="w-full bg-red-600">
+    <div className="w-full">
       <div className="w-[80%] bg-gray-300 mx-auto py-1">
         <div className="flex justify-center gap-5 w-full ">
           <Link
             className="text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded"
-            href=""
+            href="/"
           >
             হোম
           </Link>
@@ -19,35 +19,44 @@ const Navbar = () => {
             className="text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded"
             href=""
           >
-            {" "}
             কাস্টমার সার্ভিস
           </Link>
           <Link
-            className={`${pathname=='/Admins/SiteAdmin' && "bg-gray-900 text-white"} text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
+            className={`${
+              pathname == "/Admins/SiteAdmin" && "bg-gray-900 text-white"
+            } text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
             href="/Admins/SiteAdmin"
           >
             সাইট এডমিন
           </Link>
           <Link
-            className={`${pathname==='/Admins/Admin' && "bg-gray-900 text-white"} text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
+            className={`${
+              pathname === "/Admins/Admin" && "bg-gray-900 text-white"
+            } text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
             href="/Admins/Admin"
           >
             এডমিন
           </Link>
           <Link
-            className={`${pathname==='/Admins/SubAdmin' && "bg-gray-900 text-white"} text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
+            className={`${
+              pathname === "/Admins/SubAdmin" && "bg-gray-900 text-white"
+            } text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
             href="/Admins/SubAdmin"
           >
             সাব-এডমিন
           </Link>
           <Link
-            className={`${pathname==='/Admins/SuperAgent' && "bg-gray-900 text-white"} text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
+            className={`${
+              pathname === "/Admins/SuperAgent" && "bg-gray-900 text-white"
+            } text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
             href="/Admins/SuperAgent"
           >
             সুপার-এজেন্ট
           </Link>
           <Link
-            className={`${pathname==='/Admins/Agent' && "bg-gray-900 text-white"} text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
+            className={`${
+              pathname === "/Admins/Agent" && "bg-gray-900 text-white"
+            } text-base md:text-xl font-bold  text-gray-700 hover:bg-gray-700 hover:text-white px-4 py-2 rounded`}
             href="/Admins/Agent"
           >
             এজেন্ট
